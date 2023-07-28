e2b3ed44f4178da801452fd8dba7b05b16f31a98
Pavani Kottu <pavani.kottu@ratnaglobaltech.com>
2023-07-28 15:01:55 +0530

    pattern & numbers

---
+++ pattern.php
@@ -0,0 +1,26 @@
+<?php 
+//pattern 
+for($i=1; $i<=5; $i++){  
+for($j=1; $j<=$i; $j++){  
+echo ' * ';  
+}  
+echo '<br>';  
+
+}  
+
+for($p=5; $p>=1; $p--){  
+for($r=1; $r<=$p; $r++){  
+echo ' * ';  
+}  
+echo '<br>';  
+}  
+
+//Numbers
+for($i=1; $i <=10 ; $i++) {
+    for($j=1; $j<=10;$j++){
+     echo $i*$j." ";
+    }
+    echo "<br>";
+}
+
+ 
